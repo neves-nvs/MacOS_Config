b4bfccd18a145531220bf9741a885fb2c7afef3c
Diogo Ferreira <neves@M1-Neves.Home>
2023-08-30 13:50:26 +0100

    first commit

---
+++ preferences.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+# https://macos-defaults.com/
+
+# Boot
+
+# Disable the sound effects on boot
+if [ ! "$( nvram -p | grep StartupMute | awk '{print $2}' )" = '%01' ]; then
+    echo "Disabling the sound effects on boot"
+    sudo nvram StartupMute=%01
+fi
+
+# MENU BAR
+
+# Display Battery Percentage in Battery Icon
+# defaults writeocom.apple.menuextra.battery ShowPercent -string "YES"
+menuReload=false
+
+defaults -currentHost write com.apple.controlcenter.plist BatteryShowPercentage -bool true
+
+if [ $menuReload = true ]; then
+    echo "Reloading Menu Bar"
+    killall SystemUIServer
+fi
+
+# DOCK
+dockReload=false
+
+# Set Dock Icons
+# TODO
+
+if [ $dockReload = true ]; then
+    echo "Reloading Dock"
+    killall Dock
+fi
+
+# Finder
+finderReload=false
+
+# Default View Style (Nlsv - List View | icnv - Icon View | clmv - Column View | Flwv - Cover Flow View)
+# defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# show hidden files
+if [ ! "$(defaults read com.apple.finder "AppleShowAllFiles")" -eq 1 ]; then
+    echo "Enabling Finder to show hidden files"
+    defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
+    finderReload=true
+fi
+
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+
+defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME/Downloads" # TODO Not working
+
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool false
+defaults write com.apple.finder ShowPathbar -bool true # ShowPathbar apparently does not exist at first, only after togling on manually
+finderReload=true
+
+if [ $finderReload = true ]; then
+    echo "Reloading Finder"
+    killall Finder
+fi
+
+# Messages
+
+# Enable Messages via iCloud
+# TODO
+
+# Select which e-mails are usable for iMessage 
+# TODO
+
+# Screenshots
+
+# Disable shadow when capturing a window
+defaults write com.apple.screencapture "disable-shadow" -bool "false"
+
+# Non Macos Apps
+
+# Mos
+
+# reverse the mouse wheel scroll direction
+default write com.caldis.Mos reverse 1
+
+# disable smooth scrolling
+default write com.caldis.Mos smooth 0
+
+
---
+++ script.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+
+# Homebrew
+if ! command -v brew > /dev/null; then
+	echo "Installing Homebrew..."
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+	echo "Adding brew (Homebrew) to the path"
+	(echo; echo "eval '$(/opt/homebrew/bin/brew shellenv)'") >> /Users/"${USER}"/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+	echo "Homebrew is already installed."
+fi
+
+# Packages
+printf "\nInstalling packages...\n"
+packages="
+	git
+	wget
+	nvim
+	tmux
+	tmuxp
+	stow
+	exa
+	bat
+	htop
+	wakeonlan
+	ripgrep
+	ripgrep-all
+	fd
+"
+for package in $packages; do
+
+	if ! brew list "$package" > /dev/null 2>&1; then
+		brew install "$package" 
+	else
+		echo "$package is already installed."
+	fi
+done
+
+# Casks
+printf "\nInstalling casks...\n"
+casks="
+	visual-studio-code
+	iterm2
+	google-chrome
+	onedrive
+	discord
+	spotify
+	mos
+	hammerspoon
+	appcleaner
+	microsoft-word
+	microsoft-powerpoint
+	microsoft-excel
+"
+#bitwarden # Installed via App Store to have Safari Extension
+for cask in ${casks}; do
+  	# Install the Cask
+	if ! brew list --cask | grep "$cask" > /dev/null; then
+  		brew install --cask "$cask"
+	else
+		echo "$cask is already installed."
+	fi
+done
+
+# ZSH Configs
+printf "\nInstalling ZSH configs...\n"
+
+# Oh My Zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+  echo "Oh My Zsh is already installed."
+fi
+
+# ZSH autosuggestions
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions ]; then
+	git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+else
+  echo "zsh-autosuggestions is already installed."
+fi
+
+# ZSH syntax highlighting
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting ]; then
+	echo "Installing zsh-syntax-highlighting..."
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+else
+  echo "zsh-syntax-highlighting is already installed."
+fi
+
+# CONFIG (dotfiles)
+
+if [ ! -d "$HOME/.dotfiles" ]; then # TODO
+	git clone https://github.com/neves-nvs/dotfiles "$HOME/.dotfiles"
+	cd "$HOME/.dotfiles" || exit
+	./install.sh
+	cd "$HOME" || exit
+else
+	echo "dotfiles are already installed."
+fi
+
+printf "\n"
+# Hamerspoon
+
+# ControlEscape
+
+if [ ! -d ~/.hammerspoon/Spoons/ControlEscape.spoon ]; then
+	git clone https://github.com/jasonrudolph/ControlEscape.spoon.git ~/.hammerspoon/Spoons/ControlEscape.spoon
+	cd ~/.hammerspoon/Spoons/ControlEscape.spoon || exit # TODO probably return do previous directory
+	script/setup
+	# TODO still needs
+	# With one more bit of setup, you'll be able to hold caps lock for control, and tap caps lock for escape:
+	# Open System Preferences, navigate to Keyboard > Modifier Keys, and set the caps lock key to control.
+else
+	echo "ControlEscape is already installed."
+fi
+
+
+
+# Docker
+
+
+
+# Safari Extensions (Install using mas)
+# Bitwarden
+# AdBlock
+# Super Agent
+
